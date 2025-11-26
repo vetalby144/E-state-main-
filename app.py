871dd8ff40e91d7db243664672db02e96e3cd47c
vitalii <vetcros@gmail.com>
2025-11-26 06:14:03 +0200

    Add auto DB init

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from config import Config
 from sqlalchemy import or_
+from models import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
 
 # =====================================================
 #   MODELS
@@ -373,7 +376,4 @@ def create_admin():
 # =====================================================
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-
     app.run(debug=True)
